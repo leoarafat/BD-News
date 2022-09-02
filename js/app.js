ca75388ca58d2270fd8e40476f6af34fbd9111a4
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-09-03 02:24:02 +0800

    third commit added js function

--- js/app.js
+++ js/app.js
@@ -9,12 +9,26 @@ loadAllData()
 const displayAllData = allNews =>{
    const place = document.getElementById('navbar-news')
     allNews.forEach(news =>{
-        console.log(news)
+        // console.log(news)
        const div = document.createElement('div')
        div.innerHTML = `
-       <a onclick="showDetails()" class="nav-link" href="#">${news.category_name}</a>
+       <a onclick="showDetails('${news.category_id}')" class="nav-link" href="#">${news.category_name}</a>
        `   
        place.appendChild(div)
     })
     
+}
+ 
+// show details 
+const showDetails = (id) =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayDetails(data.data))
+}
+// display show detail
+const displayDetails = categoryId =>{
+    categoryId.forEach(id =>{
+        console.log(id)
+    })
 }
